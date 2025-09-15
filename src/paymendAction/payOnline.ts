0ca239f0042daef7ee144f7ca6d19f143ebf44b7
Islamkandi1 <eslamkandil097@gmail.com>
2025-09-16 00:44:40 +0300

    handle paymend on server

--- src/paymendAction/payOnline.ts
+++ src/paymendAction/payOnline.ts
@@ -1,10 +1,11 @@
+"use server"
 import axios from "axios";
 import { orderFormType } from '@/schema/order.schema';
 import { getToken } from "@/utilities/getToken/GetToken";
 
 export async function payOnlineAction(id:string,values:orderFormType) {
         const token = await getToken()
-    const {data} = await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=${location.origin}`, values, {
+    const {data} = await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=${process.env.NEXTAUTH_URL}`, values, {
         headers: {
             token: token as string
         }
