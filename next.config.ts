022eb71bf30ca066e9ae56991db8b2c8188c5d7f
Islamkandi1 <eslamkandil097@gmail.com>
2025-12-12 22:38:47 +0200

    next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images:{
-    remotePatterns:[new URL("https://cndbtgaifheduxginlre.supabase.co/storage/v1/object/public/**/**")]
+    remotePatterns:[new URL("https://ecommerce.routemisr.com/**/**")]
   }
 };
 
